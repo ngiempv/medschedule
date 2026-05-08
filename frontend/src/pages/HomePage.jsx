6fdf0c4d593e563f49a0a40692f3fce4ffbe822a
NghiemVP <nghiemvp@gmail.com>
2026-05-08 13:15:57 +0700

    create homepage section

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import HeroSection from "../components/HeroSection";
+import SpecialtySection from "../components/SpecialtySection";
+import ClinicSection from "../components/ClinicSection";
+import DoctorSection from "../components/DoctorSection";
+
+function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+
+      <SpecialtySection />
+
+      <ClinicSection />
+
+      <DoctorSection />
+    </div>
+  );
+}
+
+export default HomePage;
